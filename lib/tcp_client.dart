f93a2b4e5c0b787e87a6f680dc371911bc62abb2
ThinkingDobby <chiho1221@naver.com>
2022-08-05 20:51:19 +0900

    라이브러리 제거 전 저장

--- lib/tcp_client.dart
+++ lib/tcp_client.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 
@@ -12,22 +13,73 @@ class TCPClient extends StatefulWidget {
 }
 
 class _TCPClientState extends State<TCPClient> {
+  String _receivedData = "temp";
+
+  late BasicTestClient client;
+
+
+  @override
+  void initState() {
+    super.initState();
+    client = BasicTestClient();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text("TCP Client Test")),
+      body: Column(
+        children: <Widget>[
+          Text(_receivedData),
+          ElevatedButton(onPressed: sendBtnClicked, child: const Text("전송"))
+        ]
+      )
+    );
+  }
+
+  void sendBtnClicked() {
+    client.sendRequest();
+    client.sendData();
   }
 }
 
 class BasicTestClient {
-  String host = "127.0.0.1";
-  int port = 10001;
-  int bufSize = 1024;
+  String _host = "127.0.0.1";
+  int _port = 10001;
+  int _bufSize = 1024;
 
   late Socket clntSocket;
 
-  void initSocket() async {
-    clntSocket = await Socket.connect(host, port);
+  void setServAddr(String host, int port) {
+    _host = host;
+    _port = port;
   }
 
+  void sendRequest() async {
+    clntSocket = await Socket.connect(_host, _port);
+  }
+
+  void sendData() async{
+    // 전송할 데이터 입력부
+    // while (true) {
+    //
+    // }
+
+    // 임시 코드 - "hello" 전달
+    clntSocket.add(utf8.encode("hello"));
+
+    clntSocket.listen((List<int> event) {
+      // 임시
+      print(utf8.decode(event));
+    });
+
+    // 임시 코드 - 5초 대기
+    await Future.delayed(const Duration(seconds: 5));
+    stopClnt();
+  }
+
+  void stopClnt() {
+    clntSocket.close();
+    print("Disconnected");
+  }
 }
