f07dbaf7c26263e035f574cc7367307cf953abd7
ThinkingDobby <chiho1221@naver.com>
2022-08-05 15:14:21 +0900

    클라이언트 기초 구현 중 임시 저장

--- lib/tcp_client.dart
+++ lib/tcp_client.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+
+import 'package:get_ip/get_ip.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 class TCPClient extends StatefulWidget {
 
@@ -12,4 +17,17 @@ class _TCPClientState extends State<TCPClient> {
   Widget build(BuildContext context) {
     return Scaffold();
   }
+}
+
+class BasicTestClient {
+  String host = "127.0.0.1";
+  int port = 10001;
+  int bufSize = 1024;
+
+  late Socket clntSocket;
+
+  void initSocket() async {
+    clntSocket = await Socket.connect(host, port);
+  }
+
 }
