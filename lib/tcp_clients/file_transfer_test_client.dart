9ebf33c5e48408a4bb51f373eac3e84cf624aeb8
ThinkingDobby <chiho1221@naver.com>
2022-08-07 19:54:38 +0900

    wav 파일 전송 확인
    <FIN>
    바이너리 파일 입출력 이용해 wav 파일 전송 구현
    도착지에서 wav 파일 재생 오류 발생
    => write 대신 add 로 데이터 전달해 해결
    <TODO>
    코드 최적화

--- lib/tcp_clients/file_transfer_test_client.dart
+++ lib/tcp_clients/file_transfer_test_client.dart
@@ -4,6 +4,8 @@ import 'package:tcp_client_test/tcp_clients/basic_test_client.dart';
 
 class FileTransferTestClient extends BasicTestClient {
   void sendFile(Uint8List data) async{
-    clntSocket.write(data);
+    // 파일 크기 먼저 전달
+    clntSocket.write(data.length);
+    clntSocket.add(data);
   }
 }
