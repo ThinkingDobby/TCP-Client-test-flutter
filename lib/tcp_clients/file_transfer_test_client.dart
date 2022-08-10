fc8e1e968896a06c1108d16f03bf35a29acb9605
ThinkingDobby <chiho1221@naver.com>
2022-08-10 10:22:16 +0900

    비동기 처리 관련 문제 임시 조치

--- lib/tcp_clients/file_transfer_test_client.dart
+++ lib/tcp_clients/file_transfer_test_client.dart
@@ -6,6 +6,9 @@ class FileTransferTestClient extends BasicTestClient {
   void sendFile(Uint8List data) async{
     // 파일 크기 먼저 전달
     clntSocket.write(data.length);
+
+    // 오류 해결 필요
+    await Future.delayed(Duration(seconds: 1));
     clntSocket.add(data);
     stopClnt();
   }
